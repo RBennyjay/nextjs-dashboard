74b644390ea702266af6cb2cb932f83ede8efab7
RBennyjay <ejohn1@byupathway.edu>
2025-11-12 12:35:02 +0100

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Primary font
+export const inter = Inter({
+  subsets: ['latin'],
+});
+
+// Secondary font
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
